9f74692adf7cfc6aa1e9ff8b71fb6cd46bcbe88e
Aniket774 <aniketchaudhary774@gmail.com>
2024-10-15 13:02:24 +0530

    learn JS

---
+++ JS/index.js
@@ -0,0 +1,18 @@
+//console.log(a);
+//var a;
+/*var a=12;
+if(a>10){
+    a=30;
+    var b=10;
+    console.log(a);
+    console.log(b);
+}*/
+let a=12;
+if(a>10){
+    a=30;
+    let b=10;
+    console.log(a);
+    console.log(b);
+}
+console.log(a);
+//console.log(b);
