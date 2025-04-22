d0ef5dd7f13372d2c9d09244f6fc7a2d16c9db15
Aniket <aniketchaudhary774@gmail.com>
2025-04-22 15:50:55 +0530

    added cors

---
+++ USERAPP/Client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import ViewUsers from './components/ViewUsers'
+import Footer from './components/Footer'
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <ViewUsers/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
